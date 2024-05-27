0607f768699317ba9ed93e3ba70dd7fa5904d17a
Guilherme Catini <guilherme@catini.org>
2024-05-27 11:38:12 -0300

    agora sim

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('app-cache').then((cache) => {
             return cache.addAll([
-                '/*'
+                './*'
             ]);
         })
     );
